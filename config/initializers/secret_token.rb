ff9e1b30de5997caee47dd0c9b5b708e5aad7c2d
Lansdon Page <lansdon.page@email.wsu.edu>
2013-04-09 21:21:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstallTest::Application.config.secret_token = '4b1aac01b685a273fb3edf23f8bce42525101c81958188f08fc3abfd8854e68410dd35c2c1e8b4ff140975b016b103e9d295509111dcf5ef9fa2cec660c60b4a'
